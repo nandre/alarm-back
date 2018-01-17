dfd025524617aaa4c110dcb7e9ba94b807991e7e
Nicolas Andre <nicolas.andre@intech.lu>
2018-01-17 22:58:50 +0100

    //

--- resources/scripts/alarm.py
+++ resources/scripts/alarm.py
@@ -76,28 +76,28 @@ msg="nomessage"
 
 def main():
 #camera = picamera.PiCamera()
-while True:
-  if sys.stdin.readlines()[0]=="STOP":
-    break
-  if GPIO.input(pir):
-    a = datetime.datetime.now()
-    a = str(a)
-    a = a[0:19]
-    alert = ("Alarm at "+str(a))
-    print(alert)
-    pic = (a)+(".jpg")
-    vid = (a)+(".h264")
-    message = (alert),(pic),(vid)
-    # camera.resolution = (1024,768)
-    # camera.capture(pic)
-    # time.sleep(2)
-    # camera.resolution = (640, 480)
-    # camera.start_recording(vid)
-    # camera.wait_recording(10)
-    # camera.stop_recording()
-    #sms(to,message,hash)
-    mail(alert,pic,vid)
-    time.sleep(10) #    time.sleep(30)
+  while True:
+    if sys.stdin.readlines()[0]=="STOP":
+      break
+    if GPIO.input(pir):
+      a = datetime.datetime.now()
+      a = str(a)
+      a = a[0:19]
+      alert = ("Alarm at "+str(a))
+      print(alert)
+      pic = (a)+(".jpg")
+      vid = (a)+(".h264")
+      message = (alert),(pic),(vid)
+      # camera.resolution = (1024,768)
+      # camera.capture(pic)
+      # time.sleep(2)
+      # camera.resolution = (640, 480)
+      # camera.start_recording(vid)
+      # camera.wait_recording(10)
+      # camera.stop_recording()
+      #sms(to,message,hash)
+      mail(alert,pic,vid)
+      time.sleep(10) #    time.sleep(30)
 
 
 
