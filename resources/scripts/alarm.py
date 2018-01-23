7df711e09e83e7d69648b30242588fab80c2eb89
Nicolas Andre <nicolas.andre@intech.lu>
2018-01-23 19:03:59 +0100

    feat(add camera to mail)

--- resources/scripts/alarm.py
+++ resources/scripts/alarm.py
@@ -23,14 +23,15 @@ def mail(message, f=None,f2=None):
   msg['To'] = 'nicolas.andre54@gmail.com'
   msg['Subject'] = 'Alerte intrusion : '+str(a)
   #fichier joint image
-  # msg.attach(MIMEImage(file(f).read()))
+  msg.attach(MIMEImage(file(f).read()))
   # #fichier joint video
-  # part = MIMEBase('application', 'octet-stream')
-  # part.set_payload(open(f2).read())
-  # Encoders.encode_base64(part)
-  # part.add_header('Content-Disposition','attachment; filename="%s"' % os.path.basename(f2))
-  # msg.attach(part)
-  # msg.attach(MIMEText(message))
+  part = MIMEBase('application', 'octet-stream')
+  part.set_payload(open(f2).read())
+  Encoders.encode_base64(part)
+  part.add_header('Content-Disposition','attachment; filename="%s"' % os.path.basename(f2))
+  msg.attach(part)
+  msg.attach(MIMEText(message))
+
   mailserver = smtplib.SMTP('smtp.gmail.com',587)
   mailserver.ehlo()
   mailserver.starttls()
@@ -46,7 +47,7 @@ pir = 7
 GPIO.setup(pir, GPIO.IN)
 
 def main():
-  #camera = picamera.PiCamera()
+  camera = picamera.PiCamera()
   while True:
     if GPIO.input(pir):
       a = datetime.datetime.now()
@@ -54,17 +55,18 @@ def main():
       a = a[0:19]
       alert = ("Alarm at "+str(a))
       print(alert)
+      
       pic = (a)+(".jpg")
       vid = (a)+(".h264")
       message = (alert),(pic),(vid)
-      # camera.resolution = (1024,768)
-      # camera.capture(pic)
-      # time.sleep(2)
-      # camera.resolution = (640, 480)
-      # camera.start_recording(vid)
-      # camera.wait_recording(10)
-      # camera.stop_recording()
-      #sms(to,message,hash)
+      camera.resolution = (1024,768)
+      camera.capture(pic)
+      time.sleep(2)
+      camera.resolution = (640, 480)
+      camera.start_recording(vid)
+      camera.wait_recording(10)
+      camera.stop_recording()
+
       mail(alert,pic,vid)
       time.sleep(10) #    time.sleep(30)
   return 0
